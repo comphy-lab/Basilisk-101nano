2435662fed9d1fc57b66c5e5ea65d566a8cdc8ea
Vatsal Sanjay <vatsalsanjay@gmail.com>
2025-03-21 16:00:19 +0000

    🎨 feat(postProcess): Add grid level visualization to Rayleigh-Benard AMR plot
    The changes made in this commit include:
    
    1. Added a new subplot (ax3) to the Rayleigh-Benard AMR plot to visualize the grid level.
    2. The grid level information is now extracted from the data file and plotted using a colormap.
    3. The subplot spacing and colorbar placement have been adjusted to accommodate the new grid level plot.
    4. The `lsave` scalar field has been added to the list of fields to be read from the data file.
    
    These changes enhance the visualization of the Rayleigh-Benard AMR simulation by providing an additional view of the grid level, which can be useful for understanding the adaptive mesh refinement process.

--- postProcess/getData-Rayleigh-Benard.c
+++ postProcess/getData-Rayleigh-Benard.c
@@ -16,7 +16,7 @@ char filename[80];
 int nx, ny, len;
 double xmin, ymin, xmax, ymax, Deltax, Deltay;
 
-scalar T[], vel[];
+scalar T[], vel[], lsave[];
 scalar * list = NULL;
 
 int main(int a, char const *arguments[])
@@ -26,15 +26,15 @@ int main(int a, char const *arguments[])
     fprintf(stderr, "Usage: %s <filename> <xmin> <ymin> <xmax> <ymax> <ny>\n", arguments[0]);
     return 1;
   }
+
   sprintf (filename, "%s", arguments[1]);
   xmin = atof(arguments[2]); ymin = atof(arguments[3]);
   xmax = atof(arguments[4]); ymax = atof(arguments[5]);
   ny = atoi(arguments[6]);
-  // ... rest of the code
-}
 
   list = list_add (list, T);
   list = list_add (list, vel);
+  list = list_add (list, lsave);
 
   /*
   Actual run and codes!
